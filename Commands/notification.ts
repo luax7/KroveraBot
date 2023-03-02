aef93acace728b4d3a131c477e951f842047f7d7
Luan Xavier <luanxrodrigues@gmail.com>
2023-03-02 08:47:41 -0300

    notification

--- Commands/notification.ts
+++ Commands/notification.ts
@@ -2,6 +2,7 @@ import { Command } from "ark-handler";
 import Discord from 'discord.js';
 import { ActionRowBuilder, ButtonBuilder } from '@discordjs/builders';
 import { ButtonStyle } from 'discord.js';
+import { collector } from '../index';
 
 export default {
 hidden: true,
@@ -10,25 +11,43 @@ hidden: true,
         if(Message.author.id !== "340527910689570828") return;
 
         const Channel = await Message.author.createDM(true);
-        Channel.send("\n Conteudo salvo, referencias e links")
+        const ExMessage = await (await Channel.send("\n Conteudo salvo")).reply("Por favor, envie os links ")
 
-        Channel.createMessageCollector()
+        const collector = Channel.createMessageCollector({
+            max:1,
+            time:10 * 1000 * 60
+        })
         .on("collect", (message) => {
 
-            const separatas = message.content.split(' ');
+            if(message.content === "end"){
+                collector.dispose(message)
+
+                return
+            }
+            const Doubles = message.content.split("\n")
+
             const row = new ActionRowBuilder<ButtonBuilder>()
 
-            for(const Link in separatas )
+            for(var i = 0; i < Doubles.length; i++)
             {
+                const Double = Doubles[i].split(":")
                 row.addComponents( new ButtonBuilder({
                     
                     style: ButtonStyle.Link,
                     type: Discord.ComponentType.Button,
-                    url:Link,
+                    url:Double[1],
+                    label:Double[0]
                     
                 }))
             }
 
+        }).on("dispose", () => {
+            ExMessage.delete()
+            Channel.send("Links encerrados, enviando referencias")
+        })
+        .on("end", () => {
+            ExMessage.delete()
+            Channel.send("Links encerrados, enviando referencias")
         })
 
     },
